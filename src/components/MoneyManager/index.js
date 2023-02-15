efbe97441e75a5b785bee08b25e454d2394b4e62
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-02-16 00:27:35 +0530

    updated view

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -16,7 +16,44 @@ const transactionTypeOptions = [
 // Write your code here
 
 class MoneyManager extends Component {
+  state = {
+    title: '',
+    amount: 0,
+    type: '',
+    total: 0,
+    expenses: 0,
+    balance: 0,
+    transactionsList: [],
+  }
+
+  submitDetails = event => {
+    event.preventDefault()
+    const {title, amount, type} = this.state
+    const newTransaction = {
+      title,
+      amount,
+      type,
+    }
+    this.setState(prevState => ({
+      transactionsList: [...prevState.transactionsList, newTransaction],
+    }))
+  }
+
+  setTitle = event => {
+    this.setState({title: event.target.value})
+  }
+
+  setAmount = event => {
+    this.setState({amount: event.target.value})
+  }
+
+  setType = event => {
+    this.setState({type: event.target.value})
+  }
+
   render() {
+    const {title, amount, type} = this.state
+
     return (
       <div className="bg-container">
         <div className="card-container">
@@ -30,6 +67,54 @@ class MoneyManager extends Component {
           <ul className="money-details-container">
             <MoneyDetails />
           </ul>
+          <div className="user-transactions-history-container">
+            <div className="user-transactions">
+              <h1 className="transaction-heading"> Add Transaction </h1>
+              <form className="form-control" onSubmit={this.submitDetails}>
+                <label htmlFor="title" className="title">
+                  TITLE
+                </label>
+                <input
+                  type="text"
+                  id="title"
+                  placeholder="TITLE"
+                  className="input-el"
+                  onChange={this.setTitle}
+                  value={title}
+                />
+                <label htmlFor="amount" className="title">
+                  AMOUNT
+                </label>
+                <input
+                  type="text"
+                  id="amount"
+                  placeholder="TITLE"
+                  className="input-el"
+                  onChange={this.setAmount}
+                  value={type}
+                />
+                <label className="title" htmlFor="selection">
+                  TYPE
+                </label>
+                <select
+                  className="input-el"
+                  id="selection"
+                  onChange={this.setType}
+                  value={type}
+                >
+                  {transactionTypeOptions.map(each => (
+                    <option value={each.optionId}>{each.displayText}</option>
+                  ))}
+                </select>
+                <button className="add" type="submit">
+                  Add
+                </button>
+              </form>
+            </div>
+            <div className="history-container">
+              <h1 className="transaction-heading"> History </h1>
+            </div>
+          </div>
         </div>
       </div>
     )
